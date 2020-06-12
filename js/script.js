dfd78cbca8674d87ed8890e3d8e152597b7456d1
Eben Vranken <36372346+EbenVranken@users.noreply.github.com>
2020-06-12 16:23:27 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+function one(){
+    document.getElementById("mercury").style.animation = "spin 5067014s linear infinite"; 
+    document.getElementById("venus").style.animation = "spin 20995200s linear infinite"; 
+    document.getElementById("earth").style.animation = "spin 86400s linear infinite";
+    document.getElementById("mars").style.animation = "spin 90000s linear infinite"; 
+    document.getElementById("jupiter").style.animation = "spin 36000s linear infinite";
+    document.getElementById("saturn").style.animation = "spin 39600s linear infinite";
+    document.getElementById("uranus").style.animation = "spin 61200s linear infinite";
+    document.getElementById("neptunus").style.animation = "spin 57600s linear infinite";
+    document.getElementById("pluto").style.animation = "spin 551880s linear infinite";
+}
+
+function ten(){
+    document.getElementById("mercury").style.animation = "spin 506701.4s linear infinite"; 
+    document.getElementById("venus").style.animation = "spin 2099520s linear infinite"; 
+    document.getElementById("earth").style.animation = "spin 8640s linear infinite";
+    document.getElementById("mars").style.animation = "spin 9000s linear infinite"; 
+    document.getElementById("jupiter").style.animation = "spin 3600s linear infinite";
+    document.getElementById("saturn").style.animation = "spin 3960s linear infinite";
+    document.getElementById("uranus").style.animation = "spin 6120s linear infinite";
+    document.getElementById("neptunus").style.animation = "spin 5760s linear infinite";
+    document.getElementById("pluto").style.animation = "spin 55188s linear infinite";
+}
+
+function hundred(){
+    document.getElementById("mercury").style.animation = "spin 50670.14s linear infinite"; 
+    document.getElementById("venus").style.animation = "spin 209952s linear infinite"; 
+    document.getElementById("earth").style.animation = "spin 864s linear infinite";
+    document.getElementById("mars").style.animation = "spin 900s linear infinite"; 
+    document.getElementById("jupiter").style.animation = "spin 360s linear infinite";
+    document.getElementById("saturn").style.animation = "spin 396s linear infinite";
+    document.getElementById("uranus").style.animation = "spin 612s linear infinite";
+    document.getElementById("neptunus").style.animation = "spin 576s linear infinite";
+    document.getElementById("pluto").style.animation = "spin 5518.80s linear infinite";
+}
+
+function thousand(){
+    document.getElementById("mercury").style.animation = "spin 5067.014s linear infinite"; 
+    document.getElementById("venus").style.animation = "spin 20995.2s linear infinite"; 
+    document.getElementById("earth").style.animation = "spin 86.4s linear infinite";
+    document.getElementById("mars").style.animation = "spin 90s linear infinite"; 
+    document.getElementById("jupiter").style.animation = "spin 36s linear infinite";
+    document.getElementById("saturn").style.animation = "spin 39.6s linear infinite";
+    document.getElementById("uranus").style.animation = "spin 61.2s linear infinite";
+    document.getElementById("neptunus").style.animation = "spin 57.6s linear infinite";
+    document.getElementById("pluto").style.animation = "spin 551.88s linear infinite";
+}
+
+function on() {
+    document.getElementById("overlay").style.display = "flex";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
